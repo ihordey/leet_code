4fb58fc8efc9b8d2322e6b6a6a79f7fbb3b40491
ihordey <ihordey@gmail.com>
2022-05-26 12:15:20 +0300

    add ListNode structure

---
+++ src/main/java/org/leet/code/ListNodeUtils.java
@@ -0,0 +1,26 @@
+package org.leet.code;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListNodeUtils {
+    private ListNodeUtils() {
+    }
+
+    public static ListNode toListNode(int[] data) {
+        ListNode head = new ListNode(data[data.length - 1]);
+        for (int i = data.length - 2; i >= 0; i--) {
+            head = new ListNode(data[i], head);
+        }
+        return head;
+    }
+
+    public static List<Integer> toList(ListNode listNode) {
+        final List<Integer> result = new ArrayList<>(10);
+        while (listNode != null) {
+            result.add(listNode.val);
+            listNode = listNode.next;
+        }
+        return result;
+    }
+}
---
+++ src/test/java/org/leet/code/ListNodeTest.java
@@ -0,0 +1,13 @@
+package org.leet.code;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ListNodeTest {
+
+    @Test
+    void test() {
+        System.out.println();
+    }
+}
