60e1653748a22a788031b1df108f878fbc9478cf
ihordey <ihordey@gmail.com>
2022-05-27 18:32:00 +0300

    my solution of remove duplicate from sorted array - improve code

--- src/main/java/org/leet/code/RemoveDuplicatesSortedArray.java
+++ src/main/java/org/leet/code/RemoveDuplicatesSortedArray.java
@@ -7,21 +7,16 @@ public class RemoveDuplicatesSortedArray {
             return -1;
         }
         int uniqueIndex = 1;
-        boolean hasDuplicate = false;
 
-        for (int i = 0; i < data.length - 1; i++) {
-            final int h = i + 1;
-            if (data[i] == data[h]) {
-                hasDuplicate = true;
-            } else if (data[i] != data[h]) {
-                if (hasDuplicate || uniqueIndex < h) {
-                    data[uniqueIndex] = data[h];
-                    hasDuplicate = false;
+        for (int i = 1; i < data.length; i++) {
+            if (data[i] != data[i - 1]) {
+                if (uniqueIndex != i) {
+                    data[uniqueIndex] = data[i];
                 }
                 uniqueIndex++;
             }
         }
-        if((uniqueIndex != data.length)){
+        if ((uniqueIndex != data.length)) {
             for (int i = uniqueIndex; i < data.length; i++) {
                 data[i] = -1;
             }
